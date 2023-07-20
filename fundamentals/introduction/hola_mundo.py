1743d8dee5f27422512efc23f10ec42e4f775c14
Adriana Dominguez <adridominguez21@hotmail.com>
2023-07-20 16:14:56 -0400

    practico hola mundo

---
+++ fundamentals/introduction/hola_mundo.py
@@ -0,0 +1,17 @@
+print("hola, mundo")
+
+nombre = "Adriana"
+print ("¡hola, ", nombre, "!")
+
+print ("¡hola, " +nombre+"!")
+
+num = 10
+print ("¡hola, ",num,"!")
+
+print ("¡hola, " +str(num)+ "!")
+
+
+comida_uno = "lomito"
+comida_dos = "chipaguasu"
+print("Amo comer {} y {}. ".format(comida_uno, comida_dos))
+print(f"Amo comer {comida_uno} y {comida_dos}.")
